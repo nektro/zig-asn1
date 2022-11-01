495244f354530a8f926068e8121d08e18e63c805
Meghan Denny <hello@nektro.net>
2022-10-31 23:19:35 -0700

    build.zig: enable prominent_compile_errors

--- build.zig
+++ build.zig
@@ -2,8 +2,9 @@ const std = @import("std");
 const deps = @import("./deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    const target = b.standardTargetOptions(.{});
+    b.prominent_compile_errors = true;
 
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
     const tests = b.addTest("test.zig");
